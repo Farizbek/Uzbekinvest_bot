36488a3127117318a26b87a5e4c2a69fba97dd1a
Farizbek Jumaboyev <goombock@mail.ru>
2022-06-23 19:10:42 +0500

    last 23 06 2022

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 import telebot
+from telebot import types
 
 
 bot = telebot.TeleBot("5569375690:AAHqq9wMtkbyjz14kptApDZMdPZkm36wMpQ")
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Salom botimizga hush kelibsiz\nBitronta shartni talang!!!")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+bot.polling()
